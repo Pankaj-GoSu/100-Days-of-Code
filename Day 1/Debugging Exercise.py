22b63c2a7334431fbb5983f1749fa579432455f2
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2021-07-26 12:38:48 +0530

    Day1 some files added

---
+++ Day 1/Debugging Exercise.py
@@ -0,0 +1,8 @@
+# Here i write directily the correct code after debugging
+
+print("Day1 - String Manipulation")
+print('String Concatenation is done with the  "+"  sign.')
+print('e.g. print("Hello + "World")')
+print("New line can be created with a backslash and n")
+
+ 
---
+++ Day 1/String Manipulation.py
@@ -0,0 +1,7 @@
+
+print("Hello world! \nHello world \nHello world \n")
+
+
+#==== Combine String (String Concatenation)====
+
+print("Hello"+ " " +"GoSu")
